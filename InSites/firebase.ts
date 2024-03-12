365f4f3c1c731f21bcf373af92efe9cc2e514bfc
TaigaTi <tarikabirch@gmail.com>
2024-03-11 23:25:46 -0400

    Expo and firebase configuration

--- InSites/firebase.ts
+++ InSites/firebase.ts
@@ -1,7 +1,12 @@
 import { initializeApp } from '@firebase/app';
 
 const firebaseConfig = {
-  // Your Firebase config object (you can find this in your Firebase project settings)
+    apiKey: "AIzaSyCGY670zOKEdQIO6qn0r9E7iaskkAhBbUE",
+    authDomain: "insites-208d2.firebaseapp.com",
+    projectId: "insites-208d2",
+    storageBucket: "insites-208d2.appspot.com",
+    messagingSenderId: "394345433966",
+    appId: "1:394345433966:web:ecd8ba35db5291d6808f7f"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
