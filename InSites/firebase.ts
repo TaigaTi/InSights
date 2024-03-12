cb87205e1a5d9de5790c12f6e0d72fefb08cc5af
TaigaTi <tarikabirch@gmail.com>
2024-03-11 21:42:27 -0400

    Initial commit

---
+++ InSites/firebase.ts
@@ -0,0 +1,9 @@
+import { initializeApp } from '@firebase/app';
+
+const firebaseConfig = {
+  // Your Firebase config object (you can find this in your Firebase project settings)
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
