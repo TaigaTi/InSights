24234b0382effa739a0728f5c429bf12fe5be966
TaigaTi <tarikabirch@gmail.com>
2024-03-26 21:57:13 -0400

    Added analytics navigation

--- InSites/App.tsx
+++ InSites/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './pages/Home';
+import Analytics from './pages/Analytics';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Analytics" component={Analytics}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- InSites/pages/Home.tsx
+++ InSites/pages/Home.tsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+// This component serves as the main screen of InSites, presenting users with their Dashboard information after signing in. 
 
-export default function Home() {
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function Home({ navigation }) {
     return (
         <View style={styles.container}>
             <Text>Welcome to InSites!</Text>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    height: 100,
+                    padding: 20,
+                    backgroundColor: '#fff',
+                }}>
+            </View>
+            <Button
+                title="Go to Analytics"
+                onPress={() =>
+                    navigation.navigate('Analytics')
+                }
+            />
         </View>
     );
 };
