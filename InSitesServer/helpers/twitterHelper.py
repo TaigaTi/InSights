69fca647fbca1b076d800fdeb784593a7da03553
IsaiahSama <zeldaplayergl@gmail.com>
2024-04-20 17:20:51 -0400

    Began setup for a Twitter Helper

---
+++ InSitesServer/helpers/twitterHelper.py
@@ -0,0 +1,10 @@
+import tweepy
+
+class TwitterHelper():
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.Client(self.auth)
+
+    def get_followers(self):
+        return self.api.followers()
